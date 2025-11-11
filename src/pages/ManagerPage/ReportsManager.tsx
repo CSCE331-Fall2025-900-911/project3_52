47c4db0bcaa9658caeed776da9e5d5558fa3a000
Peter Wu <wupetmupet@gmail.com>
2025-11-10 19:41:59 -0600

    changed the color of run z report button to maroon

--- src/pages/ManagerPage/ReportsManager.tsx
+++ src/pages/ManagerPage/ReportsManager.tsx
@@ -203,7 +203,7 @@ export default function ReportsManager() {
                     )
                 }
                 className={`px-4 py-2 rounded-lg font-medium ${
-                    closingZ ? "bg-gray-400" : "bg-red-700 hover:bg-red-800 text-white"
+                    closingZ ? "bg-gray-400" : "bg-maroon hover:bg-red-800 text-white"
                 }`}
             >
                 {closingZ

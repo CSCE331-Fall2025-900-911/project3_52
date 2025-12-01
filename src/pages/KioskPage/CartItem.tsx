83f4279cce36ef361554c6d32cafbe361754676f
montykang <montykang2024@gmail.com>
2025-11-30 23:57:58 -0600

    minor update to CartItem to display toppings of the drink as well (for user reference)

--- src/pages/KioskPage/CartItem.tsx
+++ src/pages/KioskPage/CartItem.tsx
@@ -6,7 +6,9 @@ export default function KioskCartItem({ item, onRemove }: { item: CartItem; onRe
     <div className="mb-3 p-3 bg-gray-100 dark:bg-gray-700 rounded-lg flex">
       <div className="flex-grow">
         <h4 className="font-bold dark:text-white"><T>{item.product.product_name}</T></h4>
-        <p className="text-sm text-gray-600 dark:text-gray-400"><T>Size</T>: {item.size}<T>, Sugar</T>: {item.sugar_level}<T>, Ice</T>: {item.ice_level} </p>
+        <p className="text-sm text-gray-600 dark:text-gray-400"><T>Size</T>: {item.size}<T>, Sugar</T>: {item.sugar_level}<T>, 
+          Ice</T>: {item.ice_level} <T>, Toppings</T>: {item.toppings.length > 0 ? item.toppings : <T>N/A</T>}
+        </p>
       </div>
       <div className="text-right">
         <p className="font-bold dark:text-white">${item.final_price}</p>
